ae18f7df809906b3b604410031fb1cd7af42abbb
Matanel Mac <mmgordon.matanel@gmail.com>
2023-09-24 15:27:25 +0300

    separated to PORT const #1

--- apps/nest-app/src/main.ts
+++ apps/nest-app/src/main.ts
@@ -1,7 +1,7 @@
 import { AppModule } from './app.module';
 import { NestFactory } from '@nestjs/core';
 
-const PORT  = 4000
+const PORT = 3000;
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
