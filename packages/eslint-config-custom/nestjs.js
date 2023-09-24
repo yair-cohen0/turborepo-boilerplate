c9a0c710676d3076701a75da26a5dbe2e710b14d
Matanel Mac <mmgordon.matanel@gmail.com>
2023-09-24 15:14:45 +0300

    handled eslint configurations

--- packages/eslint-config-custom/nestjs.js
+++ packages/eslint-config-custom/nestjs.js
@@ -1,7 +1,7 @@
 module.exports = {
     extends: [
         'plugin:@typescript-eslint/recommended',
-        'plugin',
+        'prettier',
     ],
     parser: '@typescript-eslint/parser',
     parserOptions: {
