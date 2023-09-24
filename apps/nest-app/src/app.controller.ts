6b44dc3254ee30d9dd316efcc4b2d6c7339ad250
Yair Cohen <yair.cohen.mail@gmail.com>
2023-09-24 14:57:10 +0300

    pre-push check

--- apps/nest-app/src/app.controller.ts
+++ apps/nest-app/src/app.controller.ts
@@ -5,8 +5,8 @@ import { AppService } from './app.service';
 export class AppController {
     constructor(private readonly appService: AppService) {}
 
-    @Get('')
-    getHello(): string {
+    @Get()
+    getHello(): boolean {
         return this.appService.getHello();
     }
 }
